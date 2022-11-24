ded7299306be626c2196f4578f2eca4771dcf81f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-11-24 22:50:50 +0100

    show links

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,14 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate, :kind_id
 
-  belongs_to :kind
+  belongs_to :kind do 
+    link(:kind) { kind_url(object.id) }
+  end
+  
   has_many :phones
   has_one :address
 
+  link(:contact) { contact_url(object.id) } 
+  
   meta do 
     { author: "Vicente Simão" }
   end
-  
+
   def attributes(*args)
     b = super(*args)
     b[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
