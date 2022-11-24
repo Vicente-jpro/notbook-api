4e88e254fdd0d8237bd5f7ecbee97faccd2b7cbb
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-11-24 22:54:25 +0100

    kind changed to related

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,13 +2,13 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate, :kind_id
 
   belongs_to :kind do 
-    link(:kind) { kind_url(object.id) }
+    link(:related) { kind_url(object.id) }
   end
-  
+
   has_many :phones
   has_one :address
 
-  link(:contact) { contact_url(object.id) } 
+  link(:self) { contact_url(object.id) } 
   
   meta do 
     { author: "Vicente Simão" }
