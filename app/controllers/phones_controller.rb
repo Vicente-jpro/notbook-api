e850cbea28236dbf4a7f920453d6a431197a5e5f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-11-25 11:41:54 +0100

    New route added

---
+++ app/controllers/phones_controller.rb
@@ -0,0 +1,12 @@
+class PhonesController < ApplicationController
+    before_action :set_phones
+    
+    def show 
+      render json: @phones
+    end
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_phones
+      @phones = Contact.find(params[:contact_id]).phones
+    end
+end
