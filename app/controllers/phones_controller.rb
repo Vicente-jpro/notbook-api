750dc32205e520c03490d12706a2f4bc34eaf064
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-11-29 00:50:37 +0100

    Update phones

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,13 +1,26 @@
 class PhonesController < ApplicationController
     before_action :set_phones
 
+
+    # PATCH /contacts/1/phone
+    def update 
+      if @contact.phones.update(params_phones)
+        render json: @contact.phones, contact_phone_url(@contact.phones)
+      else
+      end
+    end
+    
     # GET /contacts/1/phone
     def show 
-      render json: @phones
+      render json: @contact.phones
     end
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_phones
-      @phones = Contact.find(params[:contact_id]).phones
+      @contact = Contact.find(params[:contact_id])
+    end
+
+    def params_phones 
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params) 
     end
 end
