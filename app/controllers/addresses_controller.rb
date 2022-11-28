b74b942c6fc89d213cf47a221e57ab48e2de45e9
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-11-29 00:38:54 +0100

    Variable modified

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,33 +4,33 @@ class AddressesController < ApplicationController
 
     # POST /contacts/1/address
     def create 
-      @address = Address.new(params_address)
+      @contact.address = Address.new(params_address)
 
-      if @address.save
-        render json: @address, status: :created, location: contact_address_url(@address)
+      if @contact.address.save
+        render json: @contact.address, status: :created, location: contact_address_url(@contact.address)
       else
-        render json: @address.errors, status: :unprocessable_entity  
+        render json: @contact.address.errors, status: :unprocessable_entity  
       end
     end
 
     # PATCH /contacts/1/address
     def update 
-      if @address.update(params_address)
-        render json: @address
+      if @contact.address.update(params_address)
+        render json: @contact.address
       else
-        render json: @address.errors, status: :unprocessable_entity  
+        render json: @contact.address.errors, status: :unprocessable_entity  
       end
     end
 
     # GET /contacts/1/address
     def show 
-      render json: @address
+      render json: @contact.address
     end
 
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_address
-      @address = Contact.find(params[:contact_id]).address
+      @contact = Contact.find(params[:contact_id])
     end
 
     def params_address
